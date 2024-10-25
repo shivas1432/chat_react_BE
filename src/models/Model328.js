0301e93cd2a6fcc10c31a882782f66e030b41c40
shivas1432 <shivashanker7337@gmail.com>
2024-10-25 13:53:18 +0100

    Fix database connections

---
+++ src/models/Model328.js
@@ -0,0 +1,37 @@
+﻿const db = require('../config/database');
+
+class Model328 {
+  constructor() {
+    this.tableName = 'table_328';
+  }
+
+  async findAll() {
+    const query = 'SELECT * FROM ' + this.tableName + ' ORDER BY created_at DESC';
+    return await db.query(query);
+  }
+
+  async findById(id) {
+    const query = 'SELECT * FROM ' + this.tableName + ' WHERE id = ?';
+    const result = await db.query(query, [id]);
+    return result[0];
+  }
+
+  async create(data) {
+    const query = 'INSERT INTO ' + this.tableName + ' (name, value, created_at) VALUES (?, ?, NOW())';
+    const result = await db.query(query, [data.name, data.value]);
+    return this.findById(result.insertId);
+  }
+
+  async update(id, data) {
+    const query = 'UPDATE ' + this.tableName + ' SET name = ?, value = ?, updated_at = NOW() WHERE id = ?';
+    await db.query(query, [data.name, data.value, id]);
+    return this.findById(id);
+  }
+
+  async delete(id) {
+    const query = 'DELETE FROM ' + this.tableName + ' WHERE id = ?';
+    return await db.query(query, [id]);
+  }
+}
+
+module.exports = new Model328();

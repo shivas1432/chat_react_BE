5c4bb3744894b6cc8324dad2ab2ec2d395c0b7fc
shivas1432 <shivashanker7337@gmail.com>
2024-10-30 09:54:48 +0000

    Add logging system

---
+++ src/controllers/Controller670.js
@@ -0,0 +1,59 @@
+﻿const Model670 = require('../models/Model670');
+
+class Controller670 {
+  async getAll(req, res) {
+    try {
+      const data = await Model670.findAll();
+      res.status(200).json({
+        success: true,
+        data: data
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        error: error.message
+      });
+    }
+  }
+
+  async getById(req, res) {
+    try {
+      const { id } = req.params;
+      const data = await Model670.findById(id);
+      
+      if (!data) {
+        return res.status(404).json({
+          success: false,
+          message: 'Data not found'
+        });
+      }
+      
+      res.status(200).json({
+        success: true,
+        data: data
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        error: error.message
+      });
+    }
+  }
+
+  async create(req, res) {
+    try {
+      const newData = await Model670.create(req.body);
+      res.status(201).json({
+        success: true,
+        data: newData
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        error: error.message
+      });
+    }
+  }
+}
+
+module.exports = new Controller670();

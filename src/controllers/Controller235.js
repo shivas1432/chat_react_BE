6988662310139969a39c7dc993d2028101eee26c
shivas1432 <shivashanker7337@gmail.com>
2024-11-17 10:28:47 +0000

    Fix memory optimization

---
+++ src/controllers/Controller235.js
@@ -0,0 +1,59 @@
+﻿const Model235 = require('../models/Model235');
+
+class Controller235 {
+  async getAll(req, res) {
+    try {
+      const data = await Model235.findAll();
+      res.status(200).json({
+        success: true,
+        data: data
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        error: error.message
+      });
+    }
+  }
+
+  async getById(req, res) {
+    try {
+      const { id } = req.params;
+      const data = await Model235.findById(id);
+      
+      if (!data) {
+        return res.status(404).json({
+          success: false,
+          message: 'Data not found'
+        });
+      }
+      
+      res.status(200).json({
+        success: true,
+        data: data
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        error: error.message
+      });
+    }
+  }
+
+  async create(req, res) {
+    try {
+      const newData = await Model235.create(req.body);
+      res.status(201).json({
+        success: true,
+        data: newData
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        error: error.message
+      });
+    }
+  }
+}
+
+module.exports = new Controller235();

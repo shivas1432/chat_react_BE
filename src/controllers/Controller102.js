b0029819a0cf90b41b881a838197ba653c0303dc
shivas1432 <shivashanker7337@gmail.com>
2024-11-23 10:28:40 +0000

    Add logging system

---
+++ src/controllers/Controller102.js
@@ -0,0 +1,59 @@
+﻿const Model102 = require('../models/Model102');
+
+class Controller102 {
+  async getAll(req, res) {
+    try {
+      const data = await Model102.findAll();
+      res.status(200).json({
+        success: true,
+        data: data
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        error: error.message
+      });
+    }
+  }
+
+  async getById(req, res) {
+    try {
+      const { id } = req.params;
+      const data = await Model102.findById(id);
+      
+      if (!data) {
+        return res.status(404).json({
+          success: false,
+          message: 'Data not found'
+        });
+      }
+      
+      res.status(200).json({
+        success: true,
+        data: data
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        error: error.message
+      });
+    }
+  }
+
+  async create(req, res) {
+    try {
+      const newData = await Model102.create(req.body);
+      res.status(201).json({
+        success: true,
+        data: newData
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        error: error.message
+      });
+    }
+  }
+}
+
+module.exports = new Controller102();
